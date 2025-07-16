fa0f964944eb96680f06b212b2bc324d14f02bd8
Dillon Connolly <connollydillon@gmail.com>
2025-07-16 12:58:50 -0700

    added new keybinds

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -54,9 +54,15 @@ require("lazy").setup({
   rocks = { enabled = false }
 })
 
+-- Telescope 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
+-- nvim-lspconfig
+vim.keymap.set('n', '<leader>ee', vim.diagnostic.open_float)
+
+-- nvim-tree
+vim.keymap.set('n', '<leader>ft', ":NvimTreeToggle<CR>", { desc = 'NvimTreeToggle' })
